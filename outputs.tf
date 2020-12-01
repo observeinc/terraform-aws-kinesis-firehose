36df1b2c17d14a75c95ee348fa5f741ffbb6f07b
João Taveira Araújo <joao@observeinc.com>
2020-12-01 13:56:15 -0800

    Conditionally export firehose policy
    No events can be pushed to Firehose if Kinesis Data Stream is set as a source.
    In such cases, set the output policy to null in order to signal to users that
    they're probably doing something wrong.

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "firehose_delivery_stream" {
-  description = "Kinesis Firehose"
+  description = "Kinesis Firehose delivery stream towards Observe"
   value       = aws_kinesis_firehose_delivery_stream.this
 }
 
 output "firehose_iam_policy" {
-  description = "IAM policy to publish records to Kinesis Firehose"
-  value       = aws_iam_policy.put_record
+  description = "IAM policy to publish records to Kinesis Firehose. If a Kinesis Data Stream is set as a source, no policy is provided since Firehose will not allow any other event source."
+  value       = local.enable_kinesis_source ? null : aws_iam_policy.put_record
 }
